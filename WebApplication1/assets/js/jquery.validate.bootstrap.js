8cf6591a885ac2d337ccc2d06a4c875921899134
Patrick McDonald <paddymcdonald@gmail.com>
2018-11-11 15:30:15 +0000

    Ugrade to Bootstrap 4

---
+++ WebApplication1/assets/js/jquery.validate.bootstrap.js
@@ -0,0 +1,25 @@
+﻿(function ($) {
+	var defaultOptions = {
+		validClass: 'is-valid',
+		errorClass: 'is-invalid',
+		highlight: function (element, errorClass, validClass) {
+            $(element)
+                .closest('.form-control')
+                .removeClass(validClass)
+                .addClass(errorClass);
+		},
+		unhighlight: function (element, errorClass, validClass) {
+			$(element)
+                .closest('.form-control')
+                .removeClass(errorClass)
+                .addClass(validClass);
+		}
+	};
+
+	$.validator.setDefaults(defaultOptions);
+
+	$.validator.unobtrusive.options = {
+		errorClass: defaultOptions.errorClass,
+		validClass: defaultOptions.validClass,
+	};
+})(jQuery);
